15dff52cc3cd7a8dc752fa758f4066c270d51f08
Paras Jain <jainparas249@gmail.com>
2019-04-03 14:05:54 +0530

    Update genrate_report.py

--- genrate_report.py
+++ genrate_report.py
@@ -2,13 +2,7 @@ import pandas as pd
 import pickle
 # using jenkinsapi
 from jenkinsapi.jenkins import Jenkins
-p_data=[{1:'dell',2:'lenovo',3:'hp',4:'vaio'}]
-df = pd.DataFrame(p_data)
 
-
-J = Jenkins('http://localhost:8080', username='paras', password='roundglass')
-
-# create a pickle file, buid_number as name
-filename = "/home/paras/.jenkins/workspace/test_m/report.pkl"
-with open(filename ,"wb") as f:
-    pickle.dump(df,f)
+J = Jenkins('http://localhost:8080', username='srinivas', password='wildly123')
+build_no=str(J['master'].get_last_build())[-2:]
+print "current BUILD_NUMBER :",build_no
