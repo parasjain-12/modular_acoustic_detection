f9b2c1acd7105ac8fa66bfe68e4465fbf15392a8
Paras Jain <jainparas249@gmail.com>
2019-04-03 14:13:33 +0530

    Update genrate_report.py

--- genrate_report.py
+++ genrate_report.py
@@ -3,6 +3,6 @@ import pickle
 # using jenkinsapi
 from jenkinsapi.jenkins import Jenkins
 
-J = Jenkins('http://localhost:8080', username='srinivas', password='wildly123')
+J = Jenkins('http://localhost:8080', username='paras', password='roundglass')
 build_no=str(J['master'].get_last_build())[-2:]
 print "current BUILD_NUMBER :",build_no
