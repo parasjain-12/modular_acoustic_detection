f657eb3a0fa8b463475682991d3a2cba8142ce01
Paras Jain <jainparas249@gmail.com>
2019-04-03 10:48:47 +0530

    Update genrate_report.py

--- genrate_report.py
+++ genrate_report.py
@@ -2,6 +2,8 @@ import pandas as pd
 import pickle
 # using jenkinsapi
 from jenkinsapi.jenkins import Jenkins
+p_data=[{1:'dell',2:'lenovo',3:'hp',4:'vaio'}]
+df = pd.DataFrame(p_data)
 
 
 J = Jenkins('http://localhost:8080', username='paras', password='roundglass')
