c7ec50c6667cf3c76c179fd1c21deee76a3dcb35
Paras Jain <jainparas249@gmail.com>
2019-04-03 15:15:39 +0530

    Update read_report.py

--- read_report.py
+++ read_report.py
@@ -9,7 +9,7 @@ build_no=str(J['test_m'].get_last_build())[-2:]
 print "current BUILD_NUMBER :",build_no
 
 # to read pickle file of current build
-filename = "home/paras/.jenkins/workspace/test_m/%s.pkl" %build_no
+filename = "/home/paras/.jenkins/workspace/test_m/%s.pkl" %build_no
 with open(filename ,"rb") as f1:
     data1=pickle.load(f1)
 print "\ncurrent BUILD data :",new_data1
@@ -19,7 +19,7 @@ last_build_no=str(J['test_m'].get_last_good_build())[-2:]
 print "Previous BUILD_NUMBER :",last_build_no
 
 # to read pickle file of previous build
-filename = "home/paras/.jenkins/workspace/test_m/%s.pkl" %last_build_no
+filename = "/home/paras/.jenkins/workspace/test_m/%s.pkl" %last_build_no
 with open(filename ,"rb") as f2:
     data2=pickle.load(f2)
 print "\nprevious BUILD data :",new_data2
